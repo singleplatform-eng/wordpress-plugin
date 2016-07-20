8c269214fcacadc37762c4b3812f67df4b8ec760
Ben Henschel <bhenschel@singleplatform.com>
2016-07-20 14:12:13 -0400

    WIP added settings

--- singleplatform.php
+++ singleplatform.php
@@ -58,22 +58,22 @@ function singlePlatformShortcode() {
 
     $html .= "<script>
                 var options = {};
-                options['PrimaryBackgroundColor'] = '#d9d9d9';
-                options['MenuDescBackgroundColor'] = '#d9d9d9';
-                options['SectionTitleBackgroundColor'] = '#f1f1f1';
-                options['SectionDescBackgroundColor'] = '#f1f1f1';
-                options['ItemBackgroundColor'] = '#ffffff';
-                options['PrimaryFontFamily'] = '" . get_option( 'sp-primary-font-family', 'Roboto' ) . "';
-                options['BaseFontSize'] = '15px';
-                options['FontCasing'] = 'Default';
-                options['PrimaryFontColor'] = '" . get_option( 'sp-primary-font-color', '#000000' ) . "';
-                options['MenuDescFontColor'] = '#000000';
-                options['SectionTitleFontColor'] = '#555555';
-                options['SectionDescFontColor'] = '#555555';
-                options['ItemTitleFontColor'] = '#555555';
-                options['FeedbackFontColor'] = '#555555';
-                options['ItemDescFontColor'] = '#555555';
-                options['ItemPriceFontColor'] = '#555555';";
+                options['PrimaryBackgroundColor'] = '" . get_option('sp-primary-background-color', '#d9d9d9') . "';
+                options['MenuDescBackgroundColor'] = '" . get_option('sp-primary-background-color', '#d9d9d9') . "';
+                options['SectionTitleBackgroundColor'] = '" . get_option('sp-secondary-background-color', '#f1f1f1') . "';
+                options['SectionDescBackgroundColor'] = '" . get_option('sp-secondary-background-color', '#f1f1f1') . "';
+                options['ItemBackgroundColor'] = '" . get_option('sp-tertiary-background-color', '#ffffff') . "';
+                options['PrimaryFontFamily'] = '" . get_option('sp-primary-font-family', 'Roboto') . "';
+                options['BaseFontSize'] = '" . get_option('sp-base-font-size', '15px') . "';
+                options['FontCasing'] = '" . get_option('sp-item-casing', 'Default') . "';
+                options['PrimaryFontColor'] = '" . get_option('sp-primary-font-color', '#000000') . "';
+                options['MenuDescFontColor'] = '" . get_option('sp-primary-font-color', '#000000') . "';
+                options['SectionTitleFontColor'] = '" . get_option('sp-secondary-font-color', '#555555') . "';
+                options['SectionDescFontColor'] = '" . get_option('sp-secondary-font-color', '#555555') . "';
+                options['ItemTitleFontColor'] = '" . get_option('sp-tertiary-font-color', '#555555') . "';
+                options['FeedbackFontColor'] = '" . get_option('sp-tertiary-font-color', '#555555') . "';
+                options['ItemDescFontColor'] = '" . get_option('sp-tertiary-font-color', '#555555') . "';
+                options['ItemPriceFontColor'] = '" . get_option('sp-tertiary-font-color', '#555555') . "';";
     $html .= "
                 options['HideDisplayOptionPhotos'] = " . $hidePhotos . ";";
     $html .= "
@@ -101,13 +101,46 @@ add_action( 'admin_menu', function() {
     );
 
     register_setting( 'singleplatform-admin', 'sp-location-id' );
-    register_setting( 'singleplatform-admin', 'sp-primary-font-color' );
-    register_setting( 'singleplatform-admin', 'sp-display-photos' );
-    register_setting( 'singleplatform-admin', 'sp-primary-font-family' );
 
+    register_setting( 'singleplatform-admin', 'sp-primary-background-color' );
+    register_setting( 'singleplatform-admin', 'sp-secondary-background-color' );
+    register_setting( 'singleplatform-admin', 'sp-tertiary-background-color' );
+
+    register_setting( 'singleplatform-admin', 'sp-primary-font-color' );
+    register_setting( 'singleplatform-admin', 'sp-secondary-font-color' );
+    register_setting( 'singleplatform-admin', 'sp-tertiary-font-color' );
+
+    register_setting( 'singleplatform-admin', 'sp-font-family' );
+    register_setting( 'singleplatform-admin', 'sp-base-font-size' );
+    register_setting( 'singleplatform-admin', 'sp-item-casing' );
+
+    register_setting( 'singleplatform-admin', 'sp-display-announcements' );
+    register_setting( 'singleplatform-admin', 'sp-display-photos' );
+    register_setting( 'singleplatform-admin', 'sp-display-dollar-sign' );
+    register_setting( 'singleplatform-admin', 'sp-display-price' );
+    register_setting( 'singleplatform-admin', 'sp-display-disclaimer' );
+    register_setting( 'singleplatform-admin', 'sp-menu-template' );
+    register_setting( 'singleplatform-admin', 'sp-feedback-widget' );
+    register_setting( 'singleplatform-admin', 'sp-claim-location' );
+    register_setting( 'singleplatform-admin', 'sp-attribution-image' );
+    
     add_settings_section(
         'sp-section-one',
         '',
+        '',
+        'sp-plugin'
+    );
+
+    add_settings_section(
+        'sp-background-colors-section',
+        'Background colors',
+        '',
+        'sp-plugin'
+    );
+
+    add_settings_section(
+        'sp-font-section',
+        'Font',
         '',
         'sp-plugin'
     );
@@ -126,13 +159,7 @@ add_action( 'admin_menu', function() {
         'sp-plugin'
     );
 
-    add_settings_section(
-        'sp-font-section',
-        'Font',
-        '',
-        'sp-plugin'
-    );
-
+    /* Setup Fields */
     add_settings_field(
         'sp-location-id',
         'Location ID',
@@ -149,6 +176,57 @@ add_action( 'admin_menu', function() {
         'sp-section-one'
     );
 
+    /* Background Color Fields */
+    add_settings_field(
+        'sp-primary-background-color',
+        'Primary color',
+        'singleplatformOptionPrimaryBgColor',
+        'sp-plugin',
+        'sp-background-colors-section'
+    );
+
+    add_settings_field(
+        'sp-secondary-background-color',
+        'Secondary color',
+        'singleplatformOptionSecondaryBgColor',
+        'sp-plugin',
+        'sp-background-colors-section'
+    );
+
+    add_settings_field(
+        'sp-tertiary-background-color',
+        'Tertiary color',
+        'singleplatformOptionTertiaryBgColor',
+        'sp-plugin',
+        'sp-background-colors-section'
+    );
+
+    /* Font Fields */
+    add_settings_field(
+        'sp-font-family',
+        'Font Family',
+        'singlePlatformOptionFontFamily',
+        'sp-plugin',
+        'sp-font-section'
+    );
+
+    add_settings_field(
+        'sp-base-font-size',
+        'Base font size',
+        'singleplatformOptionFontSize',
+        'sp-plugin',
+        'sp-font-section'
+    );
+
+    add_settings_field(
+        'sp-item-casing',
+        'Item casing',
+        'singleplatformOptionItemCasing',
+        'sp-plugin',
+        'sp-font-section'
+    );
+
+    /* Font Color Fields */
     add_settings_field(
         'sp-primary-font-color',
         'Primary Font Color',
@@ -158,21 +236,110 @@ add_action( 'admin_menu', function() {
     );
 
     add_settings_field(
+        'sp-secondary-font-color',
+        'Secondary Font Color',
+        'singlePlatformOptionSecondaryFontColor',
+        'sp-plugin',
+        'sp-font-colors-section'
+    );
+
+    add_settings_field(
+        'sp-tertiary-font-color',
+        'Tertiary Font Color',
+        'singlePlatformOptionTertiaryFontColor',
+        'sp-plugin',
+        'sp-font-colors-section'
+    );
+
+    /* Display Fields */
+    add_settings_field(
+        'sp-display-announcements',
+        'Announcements',
+        'singleplatformOptionDisplayAnnouncements',
+        'sp-plugin',
+        'sp-display-section'
+    );
+
+    add_settings_field(
         'sp-hide-display-option-photos',
-        'Hide Photos',
+        'Photos tab',
         'singlePlatformOptionHidePhotos',
         'sp-plugin',
         'sp-display-section'
     );
 
     add_settings_field(
-        'sp-primary-font-family',
-        'Primary Font Family',
-        'singlePlatformOptionPrimaryFontFamily',
+        'sp-display-dollar-sign',
+        'Dollar sign',
+        'singleplatformOptionDisplayDollarSign',
         'sp-plugin',
-        'sp-font-section'
+        'sp-display-section'
     );
+
+    add_settings_field(
+        'sp-display-price',
+        'Price',
+        'singleplatformOptionDisplayPrice',
+        'sp-plugin',
+        'sp-display-section'
+    );
+
+    add_settings_field(
+        'sp-display-disclaimer',
+        'Disclaimer',
+        'singleplatformOptionDisplayDisclaimer',
+        'sp-plugin',
+        'sp-display-section'
+    );
+
+    add_settings_field(
+        'sp-menu-template',
+        'New menu template',
+        'singleplatformOptionMenuTemplate',
+        'sp-plugin',
+        'sp-display-section'
+    );
+
+    add_settings_field(
+        'sp-feedback-widget',
+        'Feedback widget',
+        'singleplatformOptionFeedbackWidget',
+        'sp-plugin',
+        'sp-display-section'
+    );
+
+    add_settings_field(
+        'sp-claim-location',
+        'Claim location',
+        'singleplatformOptionClaimLocation',
+        'sp-plugin',
+        'sp-display-section'
+    );
+
+    add_settings_field(
+        'sp-attribution-image',
+        'Attribution image',
+        'singleplatformOptionAttributionImage',
+        'sp-plugin',
+        'sp-display-section'
+    );    
 });
+
+function singlePlatformSettingsPage() {
+
+    echo '<header><h1>SinglePlatform Plugin</h1></header>';
+
+    settings_errors( 'general' );
+
+    $options_url = esc_url( admin_url( 'options.php' ), array( 'https', 'http' ) );
+    echo '<form method="post" action="' . $options_url . '">';
+
+    settings_fields( 'singleplatform-admin' );
+    do_settings_sections( 'sp-plugin' );
+
+    submit_button();
+    echo '</form>';
+}
 
 function singlePlatformDisplayLocationId() {
 
@@ -200,33 +367,52 @@ function singlePlatformDisplayApiKey() {
     echo $html;
 }
 
-function singlePlatformOptionPrimaryFontColor() {
-    $display_primary_font_color = get_option( 'sp-primary-font-color' );
+/*
+ * Background colors
+ */
+function singleplatformOptionPrimaryBgColor() {
+    $display_primary_bg_color = get_option('sp-primary-background-color', '#d9d9d9');
 
-    $html = '<input type="text" class="sp-color-field" id="sp-primary-font-color" name="sp-primary-font-color"';
-    if ( $display_primary_font_color ) {
-        $html .= ' value="' . esc_attr( $display_primary_font_color ) . '"';
+    $html = '<input type="text" class="sp-color-field" id="sp-primary-background-color" name="sp-primary-background-color"';
+    if ($display_primary_bg_color) {
+        $html .= ' value="' . esc_attr($display_primary_bg_color) . '"';
     }
-    $html .= '/>';
+    $html .= ' />';
 
     echo $html;
 }
 
+function singleplatformOptionSecondaryBgColor() {
+    $display_secondary_bg_color = get_option('sp-secondary-background-color', '#f1f1f1');
 
-function singlePlatformOptionHidePhotos() {
-    $display_photos = get_option( 'sp-display-photos' );
-
-    $html = '<input type="checkbox" id="sp-display-photos" name="sp-display-photos"';
-    if ( $display_photos ) {
-        $html .= ' checked';
+    $html = '<input type="text" class="sp-color-field" id="sp-secondary-background-color" name="sp-secondary-background-color"';
+    if ($display_secondary_bg_color) {
+        $html .= ' value="' . esc_attr($display_secondary_bg_color) . '"';
     }
-    $html .= '/>';
+
+    $html .= ' />';
 
     echo $html;
 }
 
-function singlePlatformOptionPrimaryFontFamily() {
-    $display_primary_font_family = get_option( 'sp-primary-font-family', 'Roboto');
+function singleplatformOptionTertiaryBgColor() {
+    $display_tertiary_bg_color = get_option('sp-tertiary-background-color', '#ffffff');
+
+    $html = '<input type="text" class="sp-color-field" id="sp-tertiary-background-color" name="sp-tertiary-background-color"';
+    if ($display_tertiary_bg_color) {
+        $html .= ' value="' . esc_attr($display_tertiary_bg_color) . '"';
+    }
+
+    $html .= ' />';
+
+    echo $html;
+}
+
+/*
+ * Fonts
+ */
+function singlePlatformOptionFontFamily() {
+    $display_font_family = get_option('sp-font-family', 'Roboto');
     $options = [
         'Arial',
         'Helvetica Neue',
@@ -245,26 +431,203 @@ function singlePlatformOptionPrimaryFontFamily() {
     foreach ($options as $option) {
         $html .= '<option value="';
         $html .= $option . '"';
-        $html .= ($display_primary_font_family == $option) ? ' selected' : '';
+        $html .= ($display_font_family == $option) ? ' selected' : '';
         $html .= '>' . $option . '</option>';
     }
-    $html .= '<select/>';
+    $html .= '</select>';
 
     echo $html;
 }
 
-function singlePlatformSettingsPage() {
+function singleplatformOptionFontSize() {
+    $display_font_size = get_option('sp-base-font-size', '15px');
 
-    echo '<header><h1>SinglePlatform Plugin</h1></header>';
+    $options = [
+        '10px',
+        '11px',
+        '12px',
+        '14px',
+        '15px',
+        '16px'
+    ];
 
-    settings_errors( 'general' );
+    $html = '<select id="sp-base-font-family" name="sp-base-font-family">';
+    foreach ($options as $option) {
+        $html .= '<option value="' . $option . '"';
+        $html .= ($display_font_size == $option) ? ' selected' : '';
+        $html .= '>' . $option . '</option>';
+    }
+    $html .= '</select>';
 
-    $options_url = esc_url( admin_url( 'options.php' ), array( 'https', 'http' ) );
-    echo '<form method="post" action="' . $options_url . '">';
+    echo $html;
+}
 
-    settings_fields( 'singleplatform-admin' );
-    do_settings_sections( 'sp-plugin' );
+function singleplatformOptionItemCasing() {
+    $display_item_casing = get_option('sp-item-casing', 'Default');
 
-    submit_button();
-    echo '</form>';
+    $options = [
+        'Default',
+        'Lowercase',
+        'Uppercase'
+    ];
+
+    $html = '<select id="sp-item-casing" name="sp-item-casing">';
+    foreach ($options as $option) {
+        $html .= '<option value="' . $option . '"';
+        $html .= ($display_item_casing == $option) ? ' selected' : '';
+        $html .= '>' . $option . '</option>';
+    }
+    $html .= '</select>';
+
+    echo $html;
+}
+
+/*
+ * Font Colors
+ */
+function singlePlatformOptionPrimaryFontColor() {
+    $display_primary_font_color = get_option('sp-primary-font-color', '#000000');
+
+    $html = '<input type="text" class="sp-color-field" id="sp-primary-font-color" name="sp-primary-font-color"';
+    if ( $display_primary_font_color ) {
+        $html .= ' value="' . esc_attr( $display_primary_font_color ) . '"';
+    }
+    $html .= '/>';
+
+    echo $html;
+}
+
+function singlePlatformOptionSecondaryFontColor() {
+    $display_secondary_font_color = get_option('sp-secondary-font-color', '#555555');
+
+    $html = '<input type="text" class="sp-color-field" id="sp-secondary-font-color" name="sp-secondary-font-color"';
+    if ( $display_secondary_font_color ) {
+        $html .= ' value="' . esc_attr( $display_secondary_font_color ) . '"';
+    }
+    $html .= '/>';
+
+    echo $html;
+}
+
+function singlePlatformOptionTertiaryFontColor() {
+    $display_tertiary_font_color = get_option('sp-tertiary-font-color', '#555555');
+
+    $html = '<input type="text" class="sp-color-field" id="sp-tertiary-font-color" name="sp-tertiary-font-color"';
+    if ( $display_tertiary_font_color ) {
+        $html .= ' value="' . esc_attr( $display_tertiary_font_color ) . '"';
+    }
+    $html .= '/>';
+
+    echo $html;
+}
+
+/*
+ * Display Options
+ */
+function singleplatformOptionDisplayAnnouncements() {
+    $display_announcements = get_option('sp-display-announcements', true);
+
+    $html = '<input type="checkbox" id="sp-display-announcements" name="sp-display-announcements"';
+    if ($display_announcements) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singlePlatformOptionHidePhotos() {
+    $display_photos = get_option('sp-display-photos', false);
+
+    $html = '<input type="checkbox" id="sp-display-photos" name="sp-display-photos"';
+    if ( $display_photos ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionDisplayDollarSign() {
+    $display_dollar_sign = get_option('sp-display-dollar-sign', true);
+
+    $html = '<input type="checkbox" id="sp-display-dollar-sign" name="sp-display-dollar-sign"';
+    if ( $display_dollar_sign ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionDisplayPrice() {
+    $display_price = get_option('sp-display-price', true);
+
+    $html = '<input type="checkbox" id="sp-display-price" name="sp-display-price"';
+    if ( $display_price ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionDisplayDisclaimer() {
+    $display_disclaimer = get_option('sp-display-disclaimer', false);
+
+    $html = '<input type="checkbox" id="sp-display-disclaimer" name="sp-display-disclaimer"';
+    if ( $display_disclaimer ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionMenuTemplate() {
+    $display_menu_template = get_option('sp-menu-template', true);
+
+    $html = '<input type="checkbox" id="sp-menu-template" name="sp-menu-template"';
+    if ( $display_menu_template ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionFeedbackWidget() {
+    $display_feedback_widget = get_option('sp-feedback-widget', true);
+
+    $html = '<input type="checkbox" id="sp-feedback-widget" name="sp-feedback-widget"';
+    if ( $display_feedback_widget ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionClaimLocation() {
+    $display_claim_location = get_option('sp-claim-location', true);
+
+    $html = '<input type="checkbox" id="sp-claim-location" name="sp-claim-location"';
+    if ( $display_claim_location ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
+}
+
+function singleplatformOptionAttributionImage() {
+    $display_attribution_image = get_option('sp-attribution-image', true);
+
+    $html = '<input type="checkbox" id="sp-attribution-image" name="sp-attribution-image"';
+    if ( $display_attribution_image ) {
+        $html .= ' checked';
+    }
+    $html .= ' />';
+
+    echo $html;
 }
